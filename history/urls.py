2764dabf059e1fcf9d18d3a43b9c3b0def841998
Dmitry <salushkin1998@bk.ru>
2021-05-31 00:42:02 +1000

    Fix misstakes in urls

--- history/urls.py
+++ history/urls.py
@@ -5,5 +5,5 @@ app_name = 'history'
 
 urlpatterns = [
     path('', views.history, name='history'),
-    path('/all', views.all_history, name='all_history'),
+    path('all/', views.all_history, name='all_history'),
 ]
